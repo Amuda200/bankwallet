08993d80fd9c6b721678f5bc53a05577af741ba5
realpaygo <ebukamaxwell001@gmail.com>
2025-06-02 15:06:22 -0700

    Update page.tsx

--- app/withdraw/payment/page.tsx
+++ app/withdraw/payment/page.tsx
@@ -90,20 +90,20 @@ export default function PaymentPage() {
           <div className="flex justify-between items-center">
             <p className="text-white flex items-center">
               <span className="bg-blue-500 text-white px-1 mr-2 text-xs">🏦</span>
-              Acc : 6955661131
+              Acc : 5043523964
             </p>
             <button
-              onClick={() => copyToClipboard("6955661131")}
+              onClick={() => copyToClipboard("5043523964")}
               className="text-green-500 hover:text-green-400"
               title="Copy account number"
             >
               <Copy size={16} />
             </button>
           </div>
-          {copiedText === "6955661131" && <p className="text-green-500 text-xs mt-1">Account number copied!</p>}
+          {copiedText === "5043523964" && <p className="text-green-500 text-xs mt-1">Account number copied!</p>}
           <p className="text-white flex items-center mt-2">
             <span className="bg-blue-500 text-white px-1 mr-2 text-xs">👤</span>
-            Name : Daniel John
+            Name : Obadiah john
           </p>
           <p className="text-white flex items-center mt-2">
             <span className="bg-blue-500 text-white px-1 mr-2 text-xs">🏦</span>
